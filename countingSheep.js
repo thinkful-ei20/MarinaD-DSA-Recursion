21d6ae3ab251662a1fab8b30937bfa945f3f1b91
Marina Dargitz <marina.dargitz@gmail.com>
2018-06-18 09:40:34 -0600

    Implement countingSheep recursive version

--- countingSheep.js
+++ countingSheep.js
@@ -1,9 +1,18 @@
-//Input:
-//Output:
-//Input to Recursive:
-//Output from Recursive: 
+//Input: number
+//Output: string : a series of console log strings:
+//a number + 'Another sheep jumps over the fence'
+//Input to Recursive: number - 1
+//Output from Recursive: the number + 'Another sheep jumps over the fence'
 
 //Recursive Version
+const countingSheep = (num) => {
+  if (num === 0){
+    return;
+  }
+  console.log(`${num} - Another sheep jumps over the fence`);
+  return countingSheep(num-1);
+};
 
+countingSheep(3);
 
 //Iterative Version
