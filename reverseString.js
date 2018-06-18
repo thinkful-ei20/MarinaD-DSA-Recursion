f25f2a15528a365afc1d02bde437cc32084bfe6a
Marina Dargitz <marina.dargitz@gmail.com>
2018-06-18 10:13:16 -0600

    Implement reverseString solution

--- reverseString.js
+++ reverseString.js
@@ -1,9 +1,25 @@
-//Input:
-//Output:
-//Input to Recursive:
-//Output from Recursive: 
+//Input: string
+//Output: string
+//Input to Recursive: string - one character
+//Output from Recursive: rest of string + character
 
 //Recursive Version
+const reverseString = str => {
+  if (!str.length){
+    return '';
+  }
+  return reverseString(str.slice(1)) + str[0];
+};
 
+console.log(reverseString('racecars'));
 
 //Iterative Version
+const reverseStringIter = str => {
+  const reverseStr = [];
+  for (let i = str.length-1; i >= 0; i --){
+    reverseStr.push(str[i]);
+  }
+  return reverseStr.join('');
+};
+
+console.log(reverseStringIter('Test'));
