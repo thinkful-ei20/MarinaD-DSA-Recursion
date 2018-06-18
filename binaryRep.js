f6225c1ad5ea17c7c172a8759157f37899f611b2
Marina Dargitz <marina.dargitz@gmail.com>
2018-06-18 12:42:58 -0600

    Implement string splitter

--- binaryRep.js
+++ binaryRep.js
@@ -4,6 +4,15 @@
 //Output from Recursive: 
 
 //Recursive Version
+const binaryRep = num => {
+  if (Math.floor(num/2) === 0){
+    return 1;
+  }
+  if (Math.floor(num/2) === 1){
+    return 0;
+  }
+  return (num%2).concat(binaryRep(Math.floor(num/2)));
+};
 
-
+console.log(binaryRep(5));
 //Iterative Version
--- stringSplitter.js
+++ stringSplitter.js
@@ -1,9 +1,34 @@
-//Input:
-//Output:
-//Input to Recursive:
-//Output from Recursive: 
+//Input: string, a separator 'test test', ' '
+//Output: array of strings ['test', 'test']
+//Input to Recursive: rest of string
+//Output from Recursive: a substring
 
-//Recursive Version
+//Recursive Version 
+const stringSplitter = (str, seperator) =>{
+  if (!str.length){ return []; }
+  const sepIndex = str.indexOf(seperator);
+  if (sepIndex === -1){ return [str]; }
+  
+  return [str.slice(0,sepIndex), ...stringSplitter(str.slice(sepIndex+1), seperator)];
+};
 
+console.log(stringSplitter('My friend', ' '));
 
-//Iterative Version
+const stringSplitterIter = (str, seperator) => {
+  let result = [];
+  let subStr = '';
+
+  for (let i = 0; i < str.length; i++){
+    if (str[i] === seperator){
+      result.push(subStr);
+      subStr = '';
+    }
+    else {
+      subStr += str[i];
+    }    
+  }
+  result.push(subStr);
+  return result;
+};
+
+console.log(stringSplitterIter('Go go', ' '));
