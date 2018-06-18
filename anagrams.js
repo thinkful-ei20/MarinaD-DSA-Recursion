671e01d5eb409f00e20ca1fd62a9e9d68ab60542
Marina Dargitz <marina.dargitz@gmail.com>
2018-06-18 16:47:36 -0600

    Implement iterative animalHierarchy

--- anagrams.js
+++ anagrams.js
@@ -1,9 +1,79 @@
-//Input:
-//Output:
-//Input to Recursive:
-//Output from Recursive: 
+//Input: string
+//Output: all combinations of string order
+//Input to Recursive: length of string -1
+//Output from Recursive: array of possibilities
+
+//eg cat
+//
+//ca t -> 1
+//c at or ta -> 2
+//0  cat or act or tac -> 3
+
+//cat
+//act
+//tac
+//tca
+//cta
+//atc
+//each letter must be in each position string.length
+
+//east
+//east
+//eats
+//et
+//e
+//e
+//e
+
+
+
+// 1 -> 1 
+// 2 -> 2
+// 3 -> 6 
+// 4 -> 24
+
+// east
+// { e: {
+//     a:{
+//       s:{
+//         t: {}
+//       },
+//       t:{
+//         s: {}
+//       }
+//     },
+//     s:,
+//     t:,
+//   },
+//   a:,
+//   s:,
+//   t:,
+// }
+
+// parent + parent.child + parent.child.child + parent.child.child.child
 
 //Recursive Version
+const anagrams = (str, pre='') => {
+  if (pre.length <= 1){
+    console.log(pre + str);
+  }
+  else {
+    for (let i = 0; i < str.length; i ++){
+      const foo = str[i]; //v
+      const before = str.slice(0, i);//fi
+      const after = str.slice(i+1); //es
+      anagrams(after+pre, before+foo);    
+    }
+  }
+};
 
 
+console.log(anagrams('cat'));
 //Iterative Version
+// const anagramsIter = str => {
+//   const charBank = str.split('');
+//   for (let i = 0; i < charBank.length; i++){
+    
+//   }
+// };
+// console.log(anagramsIter('dog'));
--- animalHierarchy.js
+++ animalHierarchy.js
@@ -1,9 +1,49 @@
-//Input:
-//Output:
-//Input to Recursive:
-//Output from Recursive: 
+//Input: an array of objects with id and parent
+//Output: an object with nested objects connecting parents and children
+//Input to Recursive: the current ID which then becomes the parent of the next tree
+//Output from Recursive: a tree of nested objects
+
+
+const animalHierarchy = [
+  {id: 'Animals', parent: null},
+  {id: 'Mammals', parent: 'Animals'},
+  {id: 'Dogs', parent:'Mammals' },
+  {id: 'Cats', parent:'Mammals' },
+  {id: 'Golden Retriever', parent: 'Dogs'},
+  {id: 'Husky', parent:'Dogs' },
+  {id: 'Bengal', parent:'Cats' }
+];
 
 //Recursive Version
+const traverse = (animalHierarchy, parent) => {
+  let node = {};
+  animalHierarchy.filter(item => item.parent === parent)
+    .forEach(item => node[item.id] = traverse(animalHierarchy, item.id));
+  return node;  
+};
 
+console.log(traverse(animalHierarchy, null));
 
 //Iterative Version
+const traverseIter = arr => {
+  const result = {};
+  const firstLvl = arr.filter(item => item.parent === null)[0];
+  const secondLvl = arr.filter(item => item.parent === firstLvl.id)[0];
+  const thirdLvl = arr.filter(item => item.parent === secondLvl.id);
+  const fourthLvl = arr.filter(item => item.parent === (thirdLvl[0].id || thirdLvl[1].id));
+  result[firstLvl.id] = {
+    [secondLvl.id] : {
+      [thirdLvl[0].id] : {
+        [fourthLvl[0].id] : {}
+      },
+      [thirdLvl[1].id] : {
+        [fourthLvl[1].id] : {}
+      }
+    }
+  };
+  
+  return result;
+
+};
+
+console.log(traverseIter(animalHierarchy));
